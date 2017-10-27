dd10e515222b80c6e4bc8aa2516bb0bcef36eebd
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-10-27 23:25:31 +0900

    squared_deltas

--- tensor.py
+++ tensor.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 
+global sess
+sess = tf.Session()
+
 def add():
     node1 = tf.constant(3.0, dtype=tf.float32)
     node2 = tf.constant(4.0)
     print(node1, node2)
 
-    sess = tf.Session()
+    global sess
     print(sess.run([node1, node2]))
 
     node3 = tf.add(node1, node2)
@@ -17,7 +20,7 @@ def test():
     a = tf.placeholder(tf.float32)
     b = tf.placeholder(tf.float32)
     adder_node = a + b
-    sess = tf.Session()
+    global sess
     print(sess.run(adder_node, {a: 3, b: 4.5}))
     print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
 
@@ -31,10 +34,16 @@ def test2():
     linear_model = W * x + b
 
     init = tf.global_variables_initializer()
-    sess = tf.Session()
+    global sess
     sess.run(init)
 
     print(sess.run(linear_model, {x: [1,2,3,4]}))
+
+    y = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - y)
+    loss = tf.reduce_sum(squared_deltas)
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
 
 # add()
 # test()
